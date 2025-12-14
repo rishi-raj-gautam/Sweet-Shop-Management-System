0190765ef336f45e356d25e2c4da1ccd1f4d6cad
Rishi Raj Gautam <162585878+rishi-raj-gautam@users.noreply.github.com>
2025-12-14 16:56:50 +0530

    feat: added dynamic port for hosting

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ const app = require("./app");
 const connectDB = require("./config/db");
 
 connectDB();
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
-);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
