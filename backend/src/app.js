c9344886526875c37c4081325866bb4ddbf75fe8
Rishi Raj Gautam <162585878+rishi-raj-gautam@users.noreply.github.com>
2025-12-14 16:35:18 +0530

    chore: initialize express server

---
+++ backend/src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use("/api/auth", require("./routes/auth.routes"));
+app.use("/api/sweets", require("./routes/sweet.routes"));
+
+module.exports = app;
